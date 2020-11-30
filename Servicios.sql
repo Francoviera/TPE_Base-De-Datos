4cd8dd8db2b308dca90d56c0c883adb346a8ac25
Francoviera <vierafranco1@gmail.com>
2020-11-29 23:29:28 -0300

    se probo hasta los servicio y anda todo

--- Servicios.sql
+++ Servicios.sql
@@ -3,7 +3,7 @@
 --b) Los posteriores comentarios sólo deben modificar la fecha de último comentario e insertar en COMENTARIO
 
 CREATE TRIGGER TR_GR05_SYNCHRONIZATION_COMENT
-    AFTER INSERT OR UPDATE OF id_usuario, id_juego, fecha_comentario
+    BEFORE INSERT OR UPDATE OF id_usuario, id_juego, fecha_comentario
     ON GR05_COMENTARIO
     FOR EACH ROW
     execute procedure FN_GR05_SYNCHRONIZATION_COMENT();
@@ -21,21 +21,27 @@ BEGIN
     WHERE id_usuario = NEW.id_usuario
       AND id_juego = NEW.id_juego;
 
-    --IF NOT(usuario AND juego) THEN
-    IF (NEW.id_usuario <> usuario AND NEW.id_juego <> juego) THEN
+    IF (usuario is null) THEN
+    --IF (NEW.id_usuario <> usuario AND NEW.id_juego <> juego) THEN
         INSERT INTO GR05_COMENTA (id_usuario, id_juego, fecha_primer_com, fecha_ultimo_com) VALUES (NEW.id_usuario,NEW.id_juego,NEW.fecha_comentario, null);
     ELSE
         UPDATE GR05_COMENTA SET fecha_ultimo_com = NEW.fecha_comentario WHERE id_juego = NEW.id_juego AND id_usuario = NEW.id_usuario;
     END IF;
+
     RETURN NEW;
-END
+END;
 $$
     LANGUAGE 'plpgsql';
+
+drop trigger TR_GR05_SYNCHRONIZATION_COMENT ON gr05_comentario cascade;
+drop function FN_GR05_SYNCHRONIZATION_COMENT;
 
 -- 2- Dado un patrón de búsqueda devolver todos los datos de el o los usuarios junto con la cantidad de
 -- juegos que ha jugado y la cantidad de votos que ha realizado
 
-CREATE OR REPLACE FUNCTION FN_G03_PATRON_BUSQUEDA_APELLIDO(pattern varchar)
+select FN_G05_MAIL_PATTERN('consectetuer@arcuvelquam.com');
+
+CREATE OR REPLACE FUNCTION FN_G05_MAIL_PATTERN(pattern varchar)
 RETURNS TABLE (
         id GR05_USUARIO.id_usuario%type,
         surname GR05_USUARIO.apellido%type,
@@ -43,13 +49,13 @@ RETURNS TABLE (
         mail GR05_USUARIO.email%type,
         id_user_type GR05_USUARIO.id_tipo_usuario%type,
         password GR05_USUARIO.password%type,
-        cant_games_played INT,
-        cant_votes  INT
+        games_played int,
+        votes  int
 )
 AS $$
 BEGIN
     RETURN QUERY SELECT
-       id_usuario, apellido, nombre, email, id_tipo_usuario, password, coalesce(cant_games_played,0) as cant_games_played, coalesce(cant_votes,0) as cant_votes
+       u.id_usuario, u.apellido, u.nombre, u.email, u.id_tipo_usuario, u.password, coalesce(cant_games_played,0)::integer as games_played, coalesce(cant_votes,0)::integer as votes
     FROM
         GR05_USUARIO u left join (
             SELECT id_usuario, COUNT(*) as cant_games_played
@@ -59,7 +65,7 @@ BEGIN
                         FROM GR05_VOTO
                         GROUP BY id_usuario) as Vote on (u.id_usuario = Vote.id_usuario)
     WHERE
-        u.email ILIKE '%'||pattern||'%';
+        u.email LIKE '%'||pattern||'%';
 
 END; $$
 LANGUAGE 'plpgsql';
--- vistas.sql
+++ vistas.sql
@@ -1,3 +1,42 @@
+SELECT *
+FROM GR05_JUEGO
+WHERE id_juego IN (SELECT id_juego
+    FROM GR05_JUEGA j
+    GROUP BY j.id_juego
+    ORDER BY COUNT(j.id_juego) DESC
+    LIMIT 10);
+
+select FN_G03_PATRON_BUSQUEDA_APELLIDO('Mckenzie');
+
+CREATE OR REPLACE FUNCTION FN_G03_PATRON_BUSQUEDA_APELLIDO(patron varchar)
+RETURNS TABLE (
+        id_usuario gr05_usuario.id_usuario%type,
+        apellido gr05_usuario.apellido%type,
+        nombre gr05_usuario.nombre%type,
+        email gr05_usuario.email%type,
+        id_tipo_usuario gr05_usuario.id_tipo_usuario%type,
+        password gr05_usuario.password%type,
+        cant_juegos_jugados INT,
+        cant_votos  INT
+)
+AS $$
+BEGIN
+    RETURN QUERY SELECT
+       u.id_usuario, apellido, nombre, email, id_tipo_usuario, password, coalesce(cant_juegos_jugados,0) as cant_juegos_jugados, coalesce(cant_votos,0) as cant_votos
+    FROM
+        gr05_usuario u left join (SELECT id_usuario, COUNT(*) as cant_juegos_jugados
+                        FROM gr05_juega
+                        GROUP BY id_usuario) as juega on (u.id_usuario = juega.id_usuario)
+            left join  (SELECT id_usuario, COUNT(*) as cant_votos
+                        FROM gr05_voto
+                        GROUP BY id_usuario) as voto on (u.id_usuario = voto.id_usuario)
+    WHERE
+        u.apellido ILIKE '%'||patron||'%';
+
+END; $$
+LANGUAGE 'plpgsql';
+
+
 --Listar todos los comentarios realizados durante el último mes descartando aquellos
 -- juegos de la Categoría “Sin Categorías”
 
@@ -11,6 +50,8 @@ WHERE g.id_juego IN (SELECT j.id_juego
                                 WHERE descripcion <> 'Sin Categoria'))
   AND fecha_comentario >=  NOW() - '1 month'::interval;
     --DATEADD(MONTH, -1, GETDATE())
+
+select * from GR05_LAST_COMENT_MONTH;
 
 -- Listar aquellos usuarios que han comentado TODOS los juegos durante el
 -- último año, teniendo en cuenta que sólo pueden comentar aquellos juegos que han jugado.
@@ -30,19 +71,29 @@ WHERE id_usuario IN (SELECT id_usuario
 -- Realizar el ranking de los 20 juegos mejor puntuados por los Usuarios.
 -- El ranking debe ser generado considerando el promedio del valor puntuado por los usuarios y que el
 -- juego hubiera sido calificado más de 5 veces
+--actualizable
+CREATE VIEW GR05_MOST_20_PLAYED_GAMES AS
+    SELECT *
+        FROM GR05_JUEGO
+        WHERE id_juego IN (SELECT id_juego
+            FROM GR05_VOTO v
+            GROUP BY v.id_juego
+            HAVING count(*) > 5
+            ORDER BY AVG(v.valor_voto) DESC
+            LIMIT 20);
 
-CREATE VIEW GR05_MOST_20_GAMES AS
-SELECT *
-FROM GR05_JUEGO
-WHERE id_juego IN (SELECT id_juego
-   FROM GR05_VOTO v
-    GROUP BY v.id_juego
-    HAVING count(*) > 5
-    ORDER BY AVG(v.valor_voto) DESC
-    LIMIT 20);
+--no actualizable
+CREATE VIEW GR05_MOST_20_PLAYED_GAMES AS
+    SELECT j.id_juego, j.nombre_juego, j.descripcion_juego, j.id_categoria
+        FROM GR05_JUEGO j JOIN GR05_VOTO v ON (j.id_juego = v.id_juego)
+        GROUP BY v.id_juego
+        HAVING count(*) > 5
+        ORDER BY AVG(v.valor_voto) DESC
+        LIMIT 20;
 
 /* LOS_10_JUEGOS_MAS_JUGADOS: Generar una vista con los 10 juegos más jugados. */
 
+--actualizable
 CREATE VIEW GR05_MOST_10_GAME AS
 SELECT *
 FROM GR05_JUEGO
@@ -51,3 +102,11 @@ WHERE id_juego IN (SELECT id_juego
     GROUP BY j.id_juego
     ORDER BY COUNT(j.id_juego) DESC
     LIMIT 10);
+
+--no actualizable
+CREATE VIEW GR05_MOST_10_GAME AS
+SELECT jo.id_juego, jo.nombre_juego, jo.descripcion_juego, jo.id_categoria
+FROM GR05_JUEGO jo JOIN gr05_juega ja on (jo.id_juego = ja.id_juego)
+    GROUP BY jo.id_juego
+    ORDER BY COUNT(jo.id_juego) DESC
+    LIMIT 10;
